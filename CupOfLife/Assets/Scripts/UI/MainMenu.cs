25acd103f48dd7c11e810c47d08b8b71e2def1dd
NateDog71 <nathjvella@gmail.com>
2020-10-07 17:38:41 +1100

    UI Elements
    + Splash Image
    + Main Menu UI + In-game Pause Menu
    + Scene Fader
    + Added some spirtes + font

---
+++ CupOfLife/Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public string levelToLoad = "";
+
+    public SceneFader sceneFader;
+
+    public void Play()
+    {
+        sceneFader.FadeTo(levelToLoad);
+    }
+
+    public void Options()
+    {
+        
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
