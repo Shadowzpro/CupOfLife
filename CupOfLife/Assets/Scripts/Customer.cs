d3b17e63a30c0fdc9503cad14c3f801a5033465f
Lauchlan Woodhall <s191690@students.aie.edu.au>
2020-10-29 16:48:47 +1100

    Added customer script and worked in test scene
    Customer script, visual representation of customers orders in my test scene

---
+++ CupOfLife/Assets/Scripts/Customer.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Customer : MonoBehaviour
+{
+    public float customerSpawnTime = 3.125f;
+    private IEnumerator customer;
+    private IEnumerator order;
+    public GameObject speechBubble;
+    public Text dialogue;
+    public Text docket;
+    private List<int> ingredientAmounts;
+    private string[] ingredients = { "Bag(s) of Coffee Beans", "Bottle(s) of Milk", "Sugar Cube(s)", "Can(s) of Oil", "Cog(s)", "Vile(s) of Green Juice", "EyeBall(s)" };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        customer = FirstCustomer(customerSpawnTime);
+        order = DialogueBox(0);
+        StartCoroutine(customer);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(dialogue.text);
+    }
+    private bool NoCustomers()
+    {
+        return GetComponent<MeshRenderer>().enabled == false;
+    }
+
+    //kinda borked
+    private IEnumerator FirstCustomer(float waitTime)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(waitTime);
+
+            if (NoCustomers())
+            {
+                Debug.Log("New Customer Spawned");
+                NewCustomer();
+                //StopCoroutine(customer);
+            }
+            else //SHOULD REACH THIS once after customer is spawned
+            {
+                Debug.Log("There is currently a customer");
+                customerSpawnTime = 6.25f;
+                StopCoroutine(customer);
+            }
+            //yield return false;
+        }
+    }
+
+    void NewCustomer()
+    {
+        GetComponent<MeshRenderer>().enabled = true;
+        StartCoroutine(order);
+        // order here?
+        // wait for drink?
+        // drink is served?
+        // react?
+        // leaves (meshrenderer)
+        // startcoroutine again?
+    }
+
+    private IEnumerator DialogueBox(float waitTime)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1.25f);
+            speechBubble.gameObject.SetActive(true);
+            ingredientAmounts = new List<int> { Random.Range(0, 3), Random.Range(0, 3), Random.Range(0, 3) };
+            //human customer
+            dialogue.text = "One cup with, " + ingredientAmounts[0] + " " + ingredients[0] + ", " + ingredientAmounts[1] + " " + ingredients[1] + ", " + ingredientAmounts[2] + " " + ingredients[2] + " please.";
+            docket.text = ingredientAmounts[0] + " x " + ingredients[0] + '\n' + ingredientAmounts[1] + " x " + ingredients[1] + '\n' + ingredientAmounts[2] + " x " + ingredients[2];
+            yield return new WaitForSeconds(6.25f);
+            speechBubble.gameObject.SetActive(false);
+            StopCoroutine(order);
+        }
+    }
+}
--- CupOfLife/Assets/Scripts/GameManager.cs
+++ CupOfLife/Assets/Scripts/GameManager.cs
@@ -11,9 +11,8 @@ public class GameManager : MonoBehaviour
     //GAME TIME STARTS AT 9AM
     public float gameTime = 9 * 60;
     public float timeMultiplier = 1.6f;
-    public float customerSpawnTime = 5f;
-    public GameObject shopCustomer;
-    private IEnumerator customer;
+    
+    
     public float GameTime
     {
         get
@@ -29,18 +28,13 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         timerText.gameObject.SetActive(false);
-        customer = NewCustomer(customerSpawnTime);
-        //NoCustomers();
-        StartCoroutine(customer);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if (NoCustomers())
-        //{
-        //    StartCoroutine(customer);
-        //}
+        
         //SETS TIMER TO ENABLED ON NEXT FRAME
         timerText.gameObject.SetActive(true);
         
@@ -52,7 +46,7 @@ public class GameManager : MonoBehaviour
         //SETS CLOCK TO 1PM ONWARDS
         if (gameTime > (13 * 60) - 0.5)
         {
-            timerText.text = string.Format("{0:D1}:{1:D2}", ((minutes / 60) % 12), (minutes % 60)) + "pm";
+            timerText.text = string.Format(" {0:D1}:{1:D2}", ((minutes / 60) % 12), (minutes % 60)) + "pm";
         }
         //SETS CLOCK TO 12PM - 12:59PM
         else if (gameTime > (12 * 60) - 0.5)
@@ -64,39 +58,13 @@ public class GameManager : MonoBehaviour
             //SETS CLOCK TO 10AM - 11:59AM
             if (gameTime < 10 * 60)
             {
-                timerText.text = string.Format("{0:D1}:{1:D2}", (minutes / 60), (minutes % 60)) + "am";
+                timerText.text = string.Format(" {0:D1}:{1:D2}", (minutes / 60), (minutes % 60)) + "am";
             }
             //SETS CLOCK TO 9AM - 9:59AM
             else
             {
                 timerText.text = string.Format("{0:D2}:{1:D2}", (minutes / 60), (minutes % 60)) + "am";
             }
-        }
-    }
-
-    private bool NoCustomers()
-    {
-        //StartCoroutine(customer);
-        return shopCustomer.gameObject.activeSelf == true;
-        //if (shopCustomer.gameObject.activeSelf == false)
-        //{
-        //    StartCoroutine(customer);
-        //}
-    }
-
-    //kinda borked
-    private IEnumerator NewCustomer(float waitTime)
-    {
-        while (true)
-        {
-            yield return new WaitForSeconds(waitTime);
-            StopCoroutine(customer);
-            if (!NoCustomers())
-            {
-                shopCustomer.SetActive(true);
-            }
-            Debug.Log("This worked?");
-            yield return false;
         }
     }
 }
